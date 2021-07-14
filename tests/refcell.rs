3c92db2efa5e7555da6050ea74cc6cbc281b66f6
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2021-07-14 10:41:02 +0100

    Refecell Test

---
+++ tests/refcell.rs
@@ -0,0 +1,28 @@
+use std::cell::RefCell;
+
+use debug2::pprint;
+
+#[test]
+fn test_refcell() {
+    let refcell = RefCell::new(5);
+    assert_eq!(
+        pprint(&refcell),
+        "RefCell {
+    value: 5,
+}"
+    );
+    let borrow = refcell.borrow_mut();
+    assert_eq!(
+        pprint(&refcell),
+        "RefCell {
+    value: <borrowed>,
+}"
+    );
+    drop(borrow);
+    assert_eq!(
+        pprint(&refcell),
+        "RefCell {
+    value: 5,
+}"
+    );
+}
