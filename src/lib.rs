a662ebcdf2f0438112b7f7961f3a162f469c2ffe
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2021-12-27 00:48:03 +0000

    feat: add dbg! macro

--- src/lib.rs
+++ src/lib.rs
@@ -472,3 +472,38 @@ impl<'a> Formatter<'a> {
         self.mode == Mode::Pretty
     }
 }
+
+/// Prints and returns the value of a given expression for quick and dirty debugging.
+///
+/// Like [`std::dbg`], but used [`crate::Debug`] instead of [`std::fmt::Debug`]
+///
+/// # Stability
+///
+/// The exact output printed by this macro should not be relied upon and is subject to future changes.
+///
+/// # Panics
+///
+/// Panics if writing to [`std::io::stderr`] fails.
+#[macro_export]
+macro_rules! dbg {
+    () => {
+        eprintln!("[{}:{}]", file!(), line!())
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            tmp => {
+                eprintln!(
+                    "[{}:{}] {} = {}",
+                    file!(),
+                    line!(),
+                    stringify!($val),
+                    $crate::pprint(&tmp)
+                );
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        $crate::dbg!($($val:expr),+)
+    };
+}
